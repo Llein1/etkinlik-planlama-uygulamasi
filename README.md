09551483bb5119e8eaa08515de733c6a91c52441
Emir Uçar <emirucar16@hotmail.com>
2026-05-09 01:30:44 +0300

    Spring Boot dosyaları olusturuldu

---
+++ README.md
@@ -0,0 +1,2 @@
+# etkinlik-planlama-uygulamasi
+Sabancı Gençlik Seferberliği / Uludağ Üniversitesi / AKSigorta İleri Düzey Java Programlama Eğitimi Bitirme Projesi Etkinlik Planlama Uygulaması
---
+++ backend/src/test/java/com/works/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.works.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
